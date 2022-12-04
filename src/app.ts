60c36230404a912569e2b9d74a270b953fafa38e
ignaspangonis <ignas.pangonis@mif.stud.vu.lt>
2022-12-04 23:26:53 +0200

    Add comments

--- src/app.ts
+++ src/app.ts
@@ -10,24 +10,27 @@ async function main() {
     await api.createData()
     await api.createInMemoryTable()
 
+    // Agreguojami duomenys (pvz. kaip 2.4, tik surasti kelio ilgį ar konversijos kainą). Nenaudokite trumpiausio kelio.
+
+    // 1. Surasti esybes pagal savybę (pvz. rasti asmenį pagal asmens kodą, rasti banko sąskaitą pagal numerį).
+
     await executeAndLog(
       "1. Get all airlines with prefix 'Airline-'",
-      `MATCH (airline:Airline) WHERE airline.name CONTAINS 'Airline-' RETURN airline.name as airlineName`,
-      ['airlineName']
+      `MATCH (airline:Airline) WHERE airline.name CONTAINS 'Airline-' RETURN airline.name as airlineName`
     )
+
+    // 2. Surasti esybes pagal ryšį (pvz. banko sąskaitas priklausančias asmeniui, banko korteles susietas su konkretaus asmens sąskaitomis).
 
     await executeAndLog(
       "2. Find airline stops for airline 'Airline-B'",
       `MATCH (airline:Airline {name: 'Airline-B'}) -[:FLIES_TO]-> (city)
-      RETURN city.name as cityName`,
-      ['cityName']
+      RETURN city.name as cityName`
     )
 
     await executeAndLog(
       "2.1. Find all airlines that fly to 'Antalya'",
       `MATCH (airline:Airline) -[:FLIES_TO]-> (city:City {name: 'Antalya'})
-      RETURN airline.name as airlineName`,
-      ['airlineName']
+      RETURN airline.name as airlineName`
     )
 
     await executeAndLog(
@@ -39,6 +42,8 @@ async function main() {
       ['airlineName']
     )
 
+    // 3. Surasti esybes susietas giliais sąryšiais (pvz. draugų draugus, visus kelius tarp Vilniaus ir Klaipėdos; visus autobusus kuriais galima nuvažiuoti iš stotelės X į stotelę Y).
+
     await executeAndLog(
       '3. Find all flights from Antalya to Cairo',
       `MATCH cities = (start:City {name:'Antalya'}) -[:FLIGHT *..2]-> (finish:City {name:'Cairo'})
@@ -49,6 +54,8 @@ async function main() {
         apoc.coll.sum([r IN relationships(cities) | r.price]) AS totalPrice
       ORDER BY numHops, totalPrice`
     )
+
+    // 4. Surasti trumpiausią kelią (pvz. surasti trumpiausią kelią tarp Vilniaus ir Klaipėdos; surasti pigiausią būdą konvertuoti iš valiutos X į valiutą Y, kuomet turima visų bankų konversijos informacija ir optimalus būdas, gali būti atlikti kelis žingsnius).
 
     await executeAndLog(
       '4. Find cheapest flight from Antalya to Cairo',
